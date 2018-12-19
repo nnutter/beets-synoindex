b9321efeb0a0a9334d242917a907765f22dc077a
Nathaniel Nutter <iam@nnutter.com>
2018-12-18 21:46:04 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# beetsplug-synoupdate
+
+Updates the Synology (music) index whenever the beets library is changed.
+
+It assumes beets is being run on Synology DiskStation Manager so synoindex is
+available.
---
+++ beetsplug/synoupdate.py
@@ -0,0 +1,24 @@
+"""Updates the Synology (music) index whenever the beets library is changed.
+
+This assumes beets is being run on Synology DiskStation Manager so synoindex is
+available.
+"""
+
+from subprocess import run
+
+from beets.plugins import BeetsPlugin
+
+
+class MyPlugin(BeetsPlugin):
+    def __init__(self):
+        super().__init__()
+        self.register_listener('item_copied', self.index_item)
+        self.register_listener('item_moved', self.index_item)
+        self.register_listener('item_linked', self.index_item)
+        self.register_listener('item_hardlinked', self.index_item)
+
+    def index_item(self, item, source, destination):
+        if str(source).find('/volume1/music/') == 0:
+            run(['synoindex', '-R', 'music', '-n', destination, source])
+        else:
+            run(['synoindex', '-R', 'music', '-a', destination])
