f30f118023fbedd8d86982db6aa3061b575720b9
Nathaniel Nutter <iam@nnutter.com>
2019-01-10 23:31:15 -0600

    Instead of detecting rename just register a different action.

--- beetsplug/synoindex.py
+++ beetsplug/synoindex.py
@@ -12,13 +12,14 @@ from beets.plugins import BeetsPlugin
 class SynoindexPlugin(BeetsPlugin):
     def __init__(self):
         super().__init__()
-        self.register_listener('item_copied', self.index_item)
-        self.register_listener('item_moved', self.index_item)
-        self.register_listener('item_linked', self.index_item)
-        self.register_listener('item_hardlinked', self.index_item)
+        self.register_listener('item_moved', self.index_move)
 
-    def index_item(self, item, source, destination):
-        if str(source).find('/volume1/music/') == 0:
-            run(['synoindex', '-R', 'music', '-n', destination, source])
-        else:
-            run(['synoindex', '-R', 'music', '-a', destination])
+        self.register_listener('item_copied', self.index_copy)
+        self.register_listener('item_linked', self.index_copy)
+        self.register_listener('item_hardlinked', self.index_copy)
+
+    def index_copy(self, item, source, destination):
+        run(['synoindex', '-R', 'music', '-a', destination])
+
+    def index_move(self, item, source, destination):
+        run(['synoindex', '-R', 'music', '-n', destination, source])
